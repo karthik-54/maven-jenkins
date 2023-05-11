3bc9073b9472e13a350d06db3e96adf52b8523ac
karthik-54 <karthikpaidimarri@gmail.com>
2023-05-11 18:22:53 +0000

    First git commit

---
+++ src/main/java/com/capg/app/App.java
@@ -0,0 +1,13 @@
+package com.capg.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! from Maven and AWS" );
+    }
+}
